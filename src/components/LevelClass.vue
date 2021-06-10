a79949c8e365f576fc2584842b823282581eb627
Chen <2423362016@qq.com>
2021-06-10 15:09:22 +0800

    dl

---
+++ src/components/LevelClass.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block">
+    <span class="demonstration">产品级别分类</span>
+    <el-cascader
+      v-model="value"
+      :options="ops"
+      @change="handleChange">
+    </el-cascader>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+              value: [],
+              ops:[],
+            };
+        },
+      methods: {
+        handleChange(value) {
+          console.log(value);
+        },
+        getDate(){
+          this.$axios.get("Config/queryAll").then((response)=>{
+            this.ops=response.data;
+          }).catch();
+        }
+      },
+      created(){
+        this.getDate();
+      },
+    };
+</script>
+
+<style scoped>
+
+</style>
